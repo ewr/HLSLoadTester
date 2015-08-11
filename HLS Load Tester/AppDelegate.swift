60d63ccf2932b484f2493e5c7bd2c6957bb401a0
Eric Richardson <e@ericrichardson.com>
2015-08-11 15:56:41 -0400

    Initial Commit

---
+++ HLS Load Tester/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HLS Load Tester
+//
+//  Created by Eric Richardson on 8/11/15.
+//  Copyright © 2015 Eric Richardson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HLS Load Tester/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HLS Load Tester
+//
+//  Created by Eric Richardson on 8/11/15.
+//  Copyright © 2015 Eric Richardson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
